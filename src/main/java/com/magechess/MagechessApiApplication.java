ee0b926166460a428478f15b9eb99e27b8015f54
gustavoheidemanndev <gustavoheidemanndev@users.noreply.github.com>
2026-04-27 18:58:24 -0300

    feat: setup projeto magechess-api com Spring Boot 3.4

---
+++ src/main/java/com/magechess/MagechessApiApplication.java
@@ -0,0 +1,11 @@
+package com.magechess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagechessApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MagechessApiApplication.class, args);
+    }
+}
